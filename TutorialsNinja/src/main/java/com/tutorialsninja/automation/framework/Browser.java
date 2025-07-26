79a2bd28807e7bc50e9ebe96e40eabacaadc1cc9
Thiago Matheus <thiagomatheus2028@gmail.com>
2025-07-26 16:15:24 -0300

    Refactor(framework): Refatoração do Browser para maior robustez e legibilidade

--- TutorialsNinja/src/main/java/com/tutorialsninja/automation/framework/Browser.java
+++ TutorialsNinja/src/main/java/com/tutorialsninja/automation/framework/Browser.java
@@ -1,7 +1,5 @@
 package com.tutorialsninja.automation.framework;
 
-import org.openqa.selenium.OutputType;
-import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -17,31 +15,39 @@ public class Browser {
 
     public static Logger log = LoggerFactory.getLogger(Browser.class);
 
+    private static final String CHROME = "chrome";
+    private static final String FIREFOX = "firefox";
+    private static final String IE = "ie";
+
     public static WebDriver startBrowser() {
+
         String browser = Base.reader.getBrowser().toLowerCase();
         log.info("Selected Browser is: {}", browser);
 
+        WebDriver driver;
         switch (browser) {
-
-            case "chrome":
+            case CHROME:
                 WebDriverManager.chromedriver().setup();
-                Base.driver = new ChromeDriver();
-                log.info("Chrome Browser is Started{}", Base.driver.hashCode());
-                return Base.driver;
-
-            case "ie":
+                driver = new ChromeDriver();
+                log.info("Chrome Browser is Started");
+                break;
+            case IE:
                 WebDriverManager.iedriver().setup();
-                Base.driver = new InternetExplorerDriver();
-                log.info("Internet Explorer Browser is Started{}", Base.driver.hashCode());
-                return Base.driver;
+                driver = new InternetExplorerDriver();
+                log.info("Internet Explorer Browser is Started{}");
+                break;
+            case FIREFOX:
+                WebDriverManager.firefoxdriver().setup();
+                driver = new FirefoxDriver();
+                log.info("Firefox Browser is Started{}");
+                break;
 
             default:
-                WebDriverManager.firefoxdriver().setup();
-                Base.driver = new FirefoxDriver();
-                log.info("Firefox Browser is Started{}", Base.driver.hashCode());
-                return Base.driver;
-        }
+                log.error("Invalid browser specified: {}", browser);
+                throw new IllegalArgumentException("Invalid browser specified: " + browser);
 
+        }
+        return driver;
     }
 
 
